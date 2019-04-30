594909532abc60de3587967a76345ed244e5d860
Tsvetan Ovedenski <dev@tsovedenski.com>
2019-04-30 17:04:36 +0200

    Fix ClrSelect

--- src/ui/ClrSelect/ClrSelect.tsx
+++ src/ui/ClrSelect/ClrSelect.tsx
@@ -9,8 +9,8 @@ type Props = {
 const ClrSelect: FunctionComponent<Props> = ({ label, options }) => {
 
   return (
-    <div className="clr-form-control">
-      <label className="clr-control-label">{label}</label>
+    <div className="clr-form-control clr-row">
+      <label className="clr-control-label clr-col-12 clr-col-md-4">{label}</label>
       <div className="clr-control-container clr-col-12 clr-col-md-8">
             <div className="clr-select-wrapper">
               <select id="select-basic" className="clr-select">
