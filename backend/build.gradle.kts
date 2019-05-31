bebe46e117cb8182a55a7bf4f16b10e608eda4d6
Tsvetan Ovedenski <dev@tsovedenski.com>
2019-05-31 08:52:13 +0200

    Perform some gradle-kts-fu
    In other words, make all projects subprojects, add common project for
    DTOs

---
+++ backend/build.gradle.kts
@@ -0,0 +1,38 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+	id("org.springframework.boot") version "2.2.0.M3"
+	id("io.spring.dependency-management") version "1.0.7.RELEASE"
+	kotlin("jvm") version "1.3.31"
+	kotlin("plugin.spring") version "1.3.31"
+}
+
+group = "com.itpiwo.cms"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_1_8
+
+repositories {
+	mavenCentral()
+	maven { url = uri("https://repo.spring.io/snapshot") }
+	maven { url = uri("https://repo.spring.io/milestone") }
+}
+
+subprojects {
+	apply(plugin = "java")
+
+	dependencies {
+		implementation("org.jetbrains.kotlin:kotlin-reflect")
+		implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	}
+
+	tasks.withType<Test> {
+		useJUnitPlatform()
+	}
+
+	tasks.withType<KotlinCompile> {
+		kotlinOptions {
+			freeCompilerArgs = listOf("-Xjsr305=strict")
+			jvmTarget = "1.8"
+		}
+	}
+}
---
+++ backend/common/build.gradle.kts
@@ -0,0 +1,2 @@
+group = "com.itpiwo.cms"
+version = "0.0.1-SNAPSHOT"
